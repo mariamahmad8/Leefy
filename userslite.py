458fe27fa17e4bd47d3e6f20767c68f374f9d903
mariamahmad8 <157257261+mariamahmad8@users.noreply.github.com>
2024-10-16 10:56:26 -0700

    Add files via upload

--- userslite.py
+++ userslite.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask, render_template, request
 
-app = Flask(__name__, template_folder='public')
+app = Flask(__name__, template_folder='templates')
 
 def createUserTable():
     conn = sqlite3.connect('user.db')
@@ -10,23 +10,46 @@ def createUserTable():
     c.execute("""CREATE TABLE IF NOT EXISTS users( 
             id INTEGER PRIMARY KEY AUTOINCREMENT, 
             username TEXT NOT NULL,
-            password TEXT NOT NULL,
-            email TEXT NOT NULL
+            email TEXT NOT NULL,
+            password TEXT NOT NULL
+            
     )""")
     conn.commit()
     conn.close()
+
+def createPlantTable(): 
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("""CREATE TABLE IF NOT EXISTS plants( 
+            id INTEGER PRIMARY KEY AUTOINCREMENT, 
+            username TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL
+            
+    )""")
+    conn.commit()
+    conn.close()
+
+
+
+
 
 # Call the function to create the user table
 createUserTable()
 
 # to connect to home page
-@app.route('/')
-def home():  # according to video, 'home' is standard naming convention, cuz home page is the first page you see
-    return render_template('./sign_up.html')
+@app.route('/signup')
+def signup():  # according to video, 'home' is standard naming convention, cuz home page is the first page you see, but I changed it to signup, cuz homepage is different
+    return render_template('sign_up.html')
+
+
+@app.route('/signin')
+def signin(): 
+    return render_template('sign_in.html')
 
 # OKAY NOW WE GOTTA GO GET /METHOD POST THINGY FROM HTML
 # since we are retrieving information, we need "cursor" from that dude's video
-@app.route("/signup", methods=['POST'])  # Fixed to use a list
+@app.route("/signup", methods=['POST', 'GET'])  # Fixed to use a list
 def user_information(): 
     username = request.form['name']
     email = request.form['email']
@@ -34,14 +57,23 @@ def user_information():
 
     # okay so now we have RETRIEVED THE information, 
     # we need to ADD into the database. remember CURSOR and EXECUTE from man
-    conn = sqlite3.connect('user.db')
-    cursor = conn.cursor()  # cursor lets us connect to the DB and edit
-    cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, password))
-    conn.commit()
-    conn.close()
 
-    print("worked")
+    if flask.request.method == 'POST':
+        #add an if statement here checking if its post
+        conn = sqlite3.connect('user.db')
+        cursor = conn.cursor()  # cursor lets us connect to the DB and edit
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, password))
+        conn.commit()
+    elif flask.request.method == 'GET': 
+        pass
+        #add an if statemetn here checking if its get instead
+        #retrieve the information
+
+    conn.close()
+    return("worked")
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+
+
